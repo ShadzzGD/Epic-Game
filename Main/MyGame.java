b9bb729a00fc0bf90c2a08e10b4b1fd4c9e943ed
Shadzz <162993252+ShadzzGD@users.noreply.github.com>
2024-12-13 18:48:01 -0500

    a

--- Main/MyGame.java
+++ Main/MyGame.java
@@ -224,6 +224,7 @@ public class MyGame extends Game  {
                                         p.eating = true;
                                 }
                         }
+                }
                 
         }
 
